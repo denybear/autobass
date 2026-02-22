651f7e37d2ef96843e99e1c4b32a4818b7c6bc54
denybear <98181634+denybear@users.noreply.github.com>
2026-02-22 16:02:53 +0100

    changes for autorun

---
+++ autobass.sh
@@ -0,0 +1,4 @@
+export GOOGLE_API_KEY=****value****
+# export XDG_RUNTIME_DIR=/run/user/%u
+# export PULSE_SERVER=unix:/run/user/%u/pulse/native
+/usr/bin/python3 /home/pi/autobass/autobass.py >> /home/pi/autobass/autobass.log 2>&1
